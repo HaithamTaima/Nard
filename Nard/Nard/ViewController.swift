011f84c7a9035b2b82cd2a54c2d9f2b4e428e2e5
rivavillageacademy <69723115+rivavillageacademy@users.noreply.github.com>
2020-08-15 18:51:50 +0400

    Add files via upload

---
+++ Nard/Nard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nard
+//
+//  Created by Muhamed Alkhatib on 15/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
